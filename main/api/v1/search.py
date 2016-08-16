8a14897f45ed1c801d0596a168ccd60dec4c662b
Christos Topaloudis <topless@gmail.com>
2016-08-16 11:15:04 +0200

    Removed logging.

--- main/api/v1/search.py
+++ main/api/v1/search.py
@@ -13,7 +13,6 @@ import model
 import util
 from main import api_v1
 
-import logging
 
 _INDEX_NAME = 'song_search'
 
@@ -21,7 +20,6 @@ _INDEX_NAME = 'song_search'
 @api_v1.resource('/search/', endpoint='api.search')
 class SearchAPI(flask_restful.Resource):
   def get(self):
-    logging.warning(util.param('search'))
     query = "name:{}".format(util.param('search'))
     results = search.Index(name=_INDEX_NAME).search(search.Query(query_string=query))
     song_keys = [ndb.Key(urlsafe=res.doc_id) for res in results.results]
@@ -32,7 +30,6 @@ class SearchAPI(flask_restful.Resource):
 @api_v1.resource('/search/create', endpoint='api.search.indexes')
 class SearchIndexesAPI(flask_restful.Resource):
   def post(self):
-    logging.warning("CReating indexes")
     deferred.defer(create_documents)
     return flask.jsonify({
       'result': 'The queues started patching all run models!!',
@@ -42,7 +39,6 @@ class SearchIndexesAPI(flask_restful.Resource):
 
 def create_documents(next_cursor=None):
   index = search.Index(name=_INDEX_NAME)
-  logging.warning("Create Documents")
   song_dbs, next_cursor = util.get_dbs(model.Song.query(), cursor=next_cursor)
   if song_dbs:
     for song_db in song_dbs:
@@ -52,8 +48,7 @@ def create_documents(next_cursor=None):
           search.TextField(name='name', value=song_db.name),
           search.TextField(name='artist', value=song_db.artist),
         ])
-      logging.warning('Song indexed')
       index.put(document)
-  logging.warning("Patched %d entities." % len(song_dbs))
+
   if next_cursor['next'] is not None:
     deferred.defer(create_documents, next_cursor['next'])
