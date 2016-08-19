ed8915243e800c36028cafba6d9c1f228635b786
Christos Topaloudis <topless@gmail.com>
2016-08-19 13:11:32 +0200

    Synchronous user_db login, and hard redirect to server side template for login.

--- main/auth/auth.py
+++ main/auth/auth.py
@@ -410,7 +410,7 @@ def signin_user_db(user_db):
   })
   flask.session.pop('auth-params', None)
   if flask_login.login_user(flask_user_db, remember=auth_params['remember']):
-    user_db.put_async()
+    user_db.put()
     return flask.redirect(util.get_next_url(auth_params['next']))
   flask.flash('Sorry, but you could not sign in.', category='danger')
   return flask.redirect(flask.url_for('welcome'))
